e841c6000fb17a0abd9896f4d7dbc72544d7be1d
Anant Patel <anant.patel@globallogic.com>
2023-09-05 23:34:42 +0530

    init

---
+++ Stuff.js
@@ -0,0 +1,124 @@
+function drawMap(ctx) {
+    let swap = false
+    for (let indexy = 0; indexy < 600; indexy += 50) {
+        for (let index = 0; index <= 1000; index += 50) {
+            ctx.fillStyle = swap ? 'green' : 'lime'
+            ctx.fillRect(index, indexy, 50, 50);
+            swap = !swap
+        }
+    }
+}
+function Drawgol(ctx) {
+    let golxy = {
+        x: (Math.floor(Math.random() * 19) ) * 50,
+        y: (Math.floor(Math.random() * 12) ) * 50
+    }
+    for (let index = 0; index < snakebody.length; index++) {
+        if (JSON.stringify(snakebody[index]) == JSON.stringify(golxy)) {
+            golxy = {
+                x: (Math.floor(Math.random() * 19) + 1) * 50,
+                y: (Math.floor(Math.random() * 12) + 1) * 50
+            }
+            index = 0
+        }
+    }
+    ctx.beginPath();
+    ctx.arc(golxy.x + 25, golxy.y + 25, 20, 0, 2 * Math.PI);
+    ctx.fillStyle = 'pink'
+    ctx.fill();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#003300';
+    ctx.stroke();
+    return golxy
+}
+function DrawSnake(ctx) {
+    for (let index = 0; index < snakebody.length; index++) {
+        ctx.beginPath();
+        ctx.arc(snakebody[index].x + 25, snakebody[index].y + 25, 20, 0, 2 * Math.PI);
+        ctx.fillStyle = 'red'
+        ctx.fill();
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = '#003300';
+        ctx.stroke();
+    }
+}
+function ClearSnake(ctx) {
+    for (let index = 0; index < snakebody.length - 1; index++) {
+        ctx.beginPath();
+        var p = ctx.getImageData(5 + snakebody[index].x, 5 + snakebody[index].y, 1, 1).data;
+        const HexColor = "#" + ((1 << 24) + (p[0] << 16) + (p[1] << 8) + p[2]).toString(16).slice(1);
+        ctx.arc(25 + snakebody[index].x, 25 + snakebody[index].y, 20, 0, 2 * Math.PI);
+        ctx.fillStyle = HexColor
+        ctx.fill();
+        ctx.lineWidth = 10;
+        ctx.strokeStyle = HexColor
+        ctx.stroke();
+        snakebody[index].x = parseInt(snakebody[index + 1].x)
+        snakebody[index].y = parseInt(snakebody[index + 1].y)
+    }
+}
+function Losser() {
+    clearInterval(Move)
+    const interval = 5000/snakebody.length
+    GameOver.play()
+    let x=0
+    const clear= setInterval(() => {
+        ctx.beginPath();
+        var p = ctx.getImageData(5 + snakebody[x].x, 5 + snakebody[x].y, 1, 1).data;
+        const HexColor = "#" + ((1 << 24) + (p[0] << 16) + (p[1] << 8) + p[2]).toString(16).slice(1);
+        ctx.arc(25 + snakebody[x].x, 25 + snakebody[x].y, 20, 0, 2 * Math.PI);
+        ctx.fillStyle = HexColor
+        ctx.fill();
+        ctx.lineWidth = 10;
+        ctx.strokeStyle = HexColor
+        ctx.stroke();
+        x+=1
+        if (x==snakebody.length) {
+            clearInterval(clear)
+            location.reload()
+        }
+    }, interval);
+
+   // alert(`HaHa Loser score is : ${score}`)
+    
+}
+function LossCheck() {
+
+    const { x, y } = snakebody[snakebody.length - 1]
+    if (x > 950 || x < 0) {
+       
+        return  Losser()
+    }
+    if (y > 550 || y < 0) {
+        
+        return Losser()
+    }
+    for (let index = 0; index < snakebody.length - 1; index++) {
+        if (snakebody[index].x == x && snakebody[index].y == y) {
+            return Losser()
+        }
+
+    }
+}
+let eat = new Audio('./sounds/eat.mp3');
+let GameOver = new Audio('./sounds/gameover.wav');
+function logic(x, y, ctx) {
+    ClearSnake(ctx)
+    snakebody[snakebody.length - 1].x += x
+    snakebody[snakebody.length - 1].y += y
+    LossCheck()
+    position.innerText = `x=${snakebody[snakebody.length - 1].x} y=${snakebody[snakebody.length - 1].y}`
+    DrawSnake(ctx)
+   
+    if (JSON.stringify(snakebody[snakebody.length - 1]) == JSON.stringify(gol)) {
+        
+        snakebody.push(gol)
+        // var p = document.getElementById("score")
+        gol = Drawgol(ctx);
+        score += 1
+        para.innerText = `your score is :${score}`
+        eat.play();
+    }
+
+
+}
---
+++ snake.js
@@ -0,0 +1,80 @@
+var canvas = document.getElementById("myCanvas");
+var para = document.getElementById("score");
+
+var position = document.getElementById("position");
+
+const ctx = canvas.getContext('2d');
+let snakebody = [];
+snakebody.push(
+    { x: 0, y: 50 },
+    { x: 50, y: 50 },
+    { x: 100, y: 50 },
+    { x: 150, y: 50 },
+)
+position.innerText = `x=${snakebody[snakebody.length - 1].x} y=${snakebody[snakebody.length - 1].y}`
+drawMap(ctx)
+let gol = Drawgol(ctx)
+DrawSnake(ctx)
+let Move;
+let score = 0
+let CanMoveX = true
+let CanMoveY = true
+const interval = 120
+document.body.addEventListener("keydown", event => {
+    switch (event.code) {
+        case 'KeyD':
+        case 'ArrowRight':
+            if (!CanMoveX) {
+                return
+            }
+            clearInterval(Move)
+            logic(50, 0, ctx, gol, score)
+            CanMoveY = true
+            CanMoveX = false
+            Move = setInterval(() => {
+                logic(50, 0, ctx, gol, score)
+            }, interval);
+            break;
+        case 'KeyA':
+        case 'ArrowLeft':
+            if (!CanMoveX) {
+                return
+            }
+            clearInterval(Move)
+            logic(-50, 0, ctx, gol, score)
+            CanMoveY = true
+            CanMoveX = false
+            Move = setInterval(() => {
+                logic(-50, 0, ctx, gol, score)
+            }, interval);
+            break;
+        case 'KeyW':
+        case 'ArrowUp':
+            if (!CanMoveY) {
+                return
+            }
+            clearInterval(Move)
+            logic(0, -50, ctx, gol, score)
+            CanMoveY = false
+            CanMoveX = true
+            Move = setInterval(() => {
+                logic(0, -50, ctx, gol, score)
+            }, interval);
+            break;
+        case 'KeyS':
+        case 'ArrowDown':
+            if (!CanMoveY) {
+                return
+            }
+            clearInterval(Move)
+            logic(0, 50, ctx, gol, score)
+            CanMoveY = false
+            CanMoveX = true
+            Move = setInterval(() => {
+                logic(0, 50, ctx, gol, score)
+            }, interval);
+            break;
+        default:
+            break;
+    }
+});
